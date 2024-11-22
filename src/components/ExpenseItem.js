7f0089ea1aefb779bffc2d4a49ccddcf4352e3a5
Kaspar <kaspar.viitkar@voco.ee>
2024-11-22 11:31:16 +0200

    created ExpenseData

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,17 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 const ExpenseItem = (props) => {
 
-    console.log(props.data)
-
     return (
-    <div className='expense-item'>
-        <div>{props.data.date.toString()}</div>
-        <div className='expense-item__description'>
-            <h2>{props.data.title}</h2>
-            <div className='expense-item__price'>{props.data.price}</div>
+        <div className='expense-item'>
+            <ExpenseDate date={props.data.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.data.title}</h2>
+                <div className='expense-item__price'>{props.data.price}</div>
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default ExpenseItem
